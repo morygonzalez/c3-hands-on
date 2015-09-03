128b81b9eda4133f8367856250753ef8d6014fe9
Hitoshi Nakashima <hitoshi.nakashima@kaizenplatform.com>
2015-09-03 10:57:09 +0900

    Get data from api

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,19 @@
 // jshint devel:true
 console.log('\'Allo \'Allo!');
+
+(function() {
+  var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      url: 'data.json',
+      mimeType: 'json'
+    }
+  });
+
+  setTimeout(function() {
+    chart.load({
+      url: '/data2.json',
+      mimeType: 'json'
+    });
+  }, 5000);
+})();
