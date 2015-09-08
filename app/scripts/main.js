d3136c2799f3cf44907a5ca739b8799cc9172755
Hitoshi Nakashima <hitoshi.nakashima@kaizenplatform.com>
2015-09-08 14:33:30 +0900

    Culumative graph

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,44 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
+'use strict';
 
 (function() {
-  var chart = c3.generate({
-    bindto: '#chart',
-    data: {
-      url: 'data.json',
-      mimeType: 'json'
-    }
+  var $xhr = $.ajax({
+    url: '/speculation.json',
+    method: 'get'
+  });
+  $xhr.done(function(data) {
+    var speculation = data.data.speculation;
+    var optimal  = calculateCulmative(_.values(speculation.optimal));
+    var actual   = calculateCulmative(_.values(speculation.actual));
+    var original = calculateCulmative(_.values(speculation.original));
+    var keys = _.keys(speculation.optimal);
+    var chart = c3.generate({
+      bindto: '#chart',
+      data: {
+        x: 'x',
+        columns: [
+          ['x'].concat(keys),
+          ['optimal'].concat(optimal),
+          ['actual'].concat(actual),
+          ['original'].concat(original)
+        ]
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%Y-%m-%d'
+          }
+        }
+      }
+    });
   });
 
-  setTimeout(function() {
-    chart.load({
-      url: '/data2.json',
-      mimeType: 'json'
-    });
-  }, 5000);
+  var calculateCulmative = function(base) {
+    return _.inject(base, function(result, n, key) {
+      var culumative = n + (result[key - 1] || 0);
+      result.push(culumative);
+      return result;
+    }, []);
+  };
 })();
